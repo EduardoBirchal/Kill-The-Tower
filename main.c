f88b906258a994ec9de2e99cf648e0a9a0b98f77
EduardoBirchal <2021951604@teiacoltec.org>
2022-08-01 20:31:35 -0300

    Comecei o programa

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct player_s {
+    
+}
+
+int main(int argc, char* argv) {
+    while(combat) {
+        printInfo();
+        turnPlayer();
+        turnEnemy();
+    }
+
+    return EXIT_SUCCESS;
+}
