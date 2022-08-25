be7f51f2eee5086d65b95b3df15b1c197dc5dd73
EduardoBirchal <104524053+EduardoBirchal@users.noreply.github.com>
2022-08-25 10:09:20 -0300

    Coloquei term.c

--- main.c
+++ main.c
@@ -1,36 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
-
-#ifdef _WIN32 || _WIN64
-    #include <Windows.h>
-#else
-    #include <unistd.h>
-#endif
-
-#define MAX_NAME 25
-#define BORDER_LEN 96
-#define MAX_OPTION 15
-#define OPTION_AMT 6
-
-
-/* ==== Definindo structs ==== */
-
-struct player_s {
-    int hpMax, hp, dmgDiceNum, dmgDice, dmgMod, atkMod, atkNum, armor;
-    char name[MAX_NAME];
-};
-
-struct enemy_s {
-    int hpMax, hp, dmgDiceNum, dmgDice, dmgMod, atkMod, atkNum, armor;
-    char name[MAX_NAME];
-};
-
-typedef struct player_s playerS;
-typedef struct enemy_s enemyS;
-
+#include "gameFuncts.h"
 
 /* ==== Funções gerais de combate ==== */
 
@@ -45,86 +13,23 @@ int rollDice(int size, int num, int mod) {
     return total+mod;
 }
 
-void requestEnter() {
-    printf("Press ENTER to continue.");
-    getchar();
-    getchar();
-}
-
-
-/* ==== Funções gerais de impressão ==== */
-
-void clearTerm() {
-    printf("\033[2J\033[1;1H"); // Só funciona com windows. É um regex que limpa o terminal, muitas vezes mais rápido que o system("clear")
-}
-
-void centerText (int len, int borderPos) {
-    for(int i=0; i<((borderPos/2) - (len/2)); i++) {
-        printf(" ");
-    }
-}
-
-void printHp (int hpMax, int hp) {
-    printf("|");
-    for(int i=1; i<=hpMax; i++) { 
-        if(i <= (hp)) {
-            printf("#");
-        }
-        else {
-            printf(" ");
-        }
-    }
-    printf("|\n\n");
-}
-
-int updateHp (playerS *player, enemyS *enemy) {
-    if(enemy->hp < 0) enemy->hp = 0;
-    if(enemy->hp > enemy->hpMax) enemy->hp = enemy->hpMax;
-    if(player->hp < 0) player->hp = 0;
-    if(player->hp > player->hpMax) player->hp = player->hpMax;
-
-    if(enemy->hp == 0) return 1;
-    if(player->hp == 0) return 2;
-    return 0;
-}
-
-int digitNum (int num) {
-    int digits = 1;
-
-    if (num > 10) {
-        return digits + digitNum(num/10);
-    }
-    else {
-        return digits;
-    }
-}
-
-void trueSleep(int ms) {
-    #ifdef _WIN32 || _WIN64
-        //Sleep(1000); // Sleep 1 segundo
-        Sleep(ms); // Sleep 0,5 segundo
-      #else
-        //sleep(1); // Sleep 1 segundo
-        usleep(ms*1000);  // Sleep 0,5 segundo (500 milisegundos)
-      #endif
-}
-
-
 /* ==== Funções de imprimir atributos ==== */
 
 void playerInfo(playerS player) {
-    int hpMaxLen = digitNum(player.hpMax); //(int) log10f(player.hpMax) + 1;
-    int hpLen = digitNum(player.hp); //(int) log10f(player.hp) + 1;
+    int hpMaxLen = digitNum(player.hpMax);
+    int hpLen = digitNum(player.hp);
     hpLen = hpLen + hpMaxLen + 4; // hpLen é o comprimento da string do valor do hp do player, que vai ser escrita depois do nome. O +4 é por causa do espaço, dos dois parênteses e da barra
 
     // Imprime o nome
     centerText(strlen(player.name) + hpLen, BORDER_LEN);
+    printf("\033[1m ");
     printf("%s (%i/%i)\n", player.name, player.hp, player.hpMax);
+    printf("\033[0m ");
 
     // Imprime o hp e os atributos
-    centerText(player.hpMax + 2, BORDER_LEN); // O +2 é por causa das duas barras
+    centerText(player.hpMax, BORDER_LEN);
     printHp(player.hpMax, player.hp);
-    printf("Attack Roll: 1d20%+i\tDamage Roll: %id%i%+i\tArmor: %i\n", player.atkMod, player.dmgDiceNum, player.dmgDice, player.dmgMod, player.armor);
+    printf("Rolagem de Ataque: \033[4m1d20%+i\033[0m\tRolagem de Dano: \033[4m%id%i%+i\033[0m\tArmadura: \033[4m%i\033[0m\n", player.atkMod, player.dmgDiceNum, player.dmgDice, player.dmgMod, player.armor);
 }
 
 void enemyInfo(enemyS enemy) {
@@ -133,19 +38,18 @@ void enemyInfo(enemyS enemy) {
     hpLen = hpLen + hpMaxLen + 4; 
 
     // Imprime a margem de cima
-    for(int i=0; i<BORDER_LEN; i++) { 
-        printf("-");
-    }
-    printf("\n");
+    printBorder();
 
     // Imprime o nome
     centerText(strlen(enemy.name) + hpLen, BORDER_LEN);
+    printf("\033[1m ");
     printf("%s (%i/%i)\n", enemy.name, enemy.hp, enemy.hpMax);
+    printf("\033[0m ");
 
     // Imprime o hp e os atributos
-    centerText(enemy.hpMax + 2, BORDER_LEN);
+    centerText(enemy.hpMax, BORDER_LEN);
     printHp(enemy.hpMax, enemy.hp);
-    printf("Attack Roll: 1d20%+i\tDamage Roll: %id%i%+i\tArmor: %i\n", enemy.atkMod, enemy.dmgDiceNum, enemy.dmgDice, enemy.dmgMod, enemy.armor);
+    printf("Rolagem de Ataque: \033[4m1d20%+i\033[0m\tRolagem de Dano: \033[4m%id%i%+i\033[0m\tArmadura: \033[4m%i\033[0m\n", enemy.atkMod, enemy.dmgDiceNum, enemy.dmgDice, enemy.dmgMod, enemy.armor);
 
 }
 
@@ -154,20 +58,14 @@ void printInfo (playerS player, enemyS enemy) {
     clearTerm(); 
 
     // Imprime a margem de cima
-    for(int i=0; i<BORDER_LEN; i++) { 
-        printf("-");
-    }
-    printf("\n");
+    printBorder();
 
     // Imprime a informação
     playerInfo(player);
     enemyInfo(enemy);
 
     // Imprime a margem de baixo
-    for(int i=0; i<BORDER_LEN; i++) { 
-        printf("-");
-    }
-    printf("\n\n");
+    printBorder();
 }
 
 
@@ -177,47 +75,69 @@ void playerAtk(playerS *player, enemyS *enemy){
     int atkRoll = rollDice(20, 1, player->atkMod);
     int dmgRoll = 0;
 
-    printf("Attack roll result (1d20%+i): %i\n\n", player->atkMod, atkRoll);
+    printSlow("Rolagem de ataque - \033[36mrolando ");
+    printf("1d20%+i", player->atkMod);
+    rollSlow(atkRoll);
+    
     if(atkRoll-player->atkMod > 17) {
-        printf("[Critical Hit] Rolling double the damage dice.\n");
+        printSlow(" \033[33;4mAcerto Critico!\033[0m Dados de dano dobrados pra esse ataque.\n\n");
         dmgRoll = rollDice(player->dmgDice, player->dmgDiceNum*2, player->dmgMod*2);
-        printf("Damage roll result (%id%i%+i): %i\n\n", player->dmgDiceNum*2, player->dmgDice, player->dmgMod*2, dmgRoll);
-        printf("Critical hit! Your blade slashes the goblin's torso with brutal strength, dealing massive damage.\n\n");
+        printSlow("Rolagem de dano - \033[36mrolando ");
+        printf("%id%i%+i", player->dmgDiceNum*2, player->dmgDice, player->dmgMod*2);
+        rollSlow(dmgRoll);
+        printSlow("\n\nSua lamina atinge o alvo com precisao brutal, causando dano massivo.\n\n");
     } 
     else if (atkRoll >= enemy->armor) {
-        printf("[Hit] Rolling damage dice.\n");
+        printSlow(" \033[33;4mAcerto!\033[0m\n\n");
         dmgRoll = rollDice(player->dmgDice, player->dmgDiceNum, player->dmgMod);
-        printf("Damage roll result (%id%i%+i): %i\n\n", player->dmgDiceNum, player->dmgDice, player->dmgMod, dmgRoll);
-        printf("You swing at the creature, who fails to evade the attack and recoils in pain.\n\n");
+        printSlow("Rolagem de dano - \033[36mrolando ");
+        printf("%id%i%+i", player->dmgDiceNum, player->dmgDice, player->dmgMod);
+        rollSlow(dmgRoll);
+        printSlow("\n\nVoce ataca a criatura, que falha em se esquivar e recua com um grito.\n\n");
     }
     else {
-        printf("[Miss] You swing downwards at your opponent, who nimbly dodges to the side. The blade meets only dirt.\n\n");
+        printSlow(" \033[33;4mFalha...\033[0m\n\nO oponente desvia agilmente do seu golpe, saltando para o lado. A lamina encontra apenas terra.\n\n");
     }
 
     enemy->hp -= dmgRoll;
+}
+
+void playerMag (playerS *player) {
+
 }
 
 
 /* ==== Funções do turno do jogador ==== */
 
 void printOptions() {
-    char options[OPTION_AMT][MAX_OPTION] = {"ATTACK", "SKILLS", "MAGIC", "INVENTORY", "ACTIONS", "CLOSE GAME"};
+    char options[OPTION_AMT][MAX_OPTION] = {"ATACAR", "HABILIDADES", "MAGIA", "INVENTARIO", "ACOES", "FECHAR JOGO"};
     int option = 0;
 
+    printf("\n");
     for(int i=0; i<OPTION_AMT; i++) {
-        printf("[%i] %s\t", i+1, options[i]);
+        printf("%s\t", options[i]);
     }
     printf("\n");
 }
 
 int readOption(playerS *player, enemyS *enemy) {
-    int option = 0;
+    int option = 1;
 
     while (1) {
-        printf("Choose an option:\n> ");
+        printf("\nEscolha uma opcao:\n> ");
         scanf("%i", &option);
         printf("\n");
 
+        /*while (1) {
+            uint8_t *state = SDL_GetKeyboardState(NULL);
+            if (state[80]) {
+                option--;
+            }
+            if (state[79]) {
+                option++;
+                printf("%i ", option);
+            }
+        }*/
         switch (option)
         {
         case 1:
@@ -272,21 +192,21 @@ int main(int argc, char** argv) {
     int battleState = 0;
 
     // Declarando criaturas
-    playerS player = {21, 21, 1, 8, 3, 5, 1, 16, "You"};
+    playerS player = {21, 21, 1, 8, 3, 5, 1, 16, "Voce"};
     enemyS enemy = {32, 32, 1, 4, 2, 4, 2, 14, "Goblin"};
 
     while (1) {
         battleState = updateHp(&player, &enemy);
+        printInfo(player, enemy);
         if(battleState) {
             if (battleState == 1) {
-                printf("The goblin falls to the ground, defeated.\n");
+                printSlow("O goblin cai no chao, derrotado.\n");
             }
             else if (battleState == 2) {
-                printf("You succumb to your wounds and black out.\n");
+                printSlow("Voce sucumbe aos seus ferimentos e desmaia.\n");
             }
             break;
         }
-        printInfo(player, enemy);
         if (turnPlayer(&player, &enemy)) {
             break;
         }
